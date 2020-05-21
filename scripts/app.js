2316a3ac7950224e30711a94f5fb57770ed38998
Idaltchion <idaltchion@gmail.com>
2020-05-21 16:47:46 -0300

    Toggle menu action implemented

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+var alternador = document.querySelector('.js-botao-chaveador');
+alternador.onclick = function() {
+    // alert('OLAAAAA');
+    var menu = document.querySelector('.js-menu');
+    menu.classList.toggle('menu--exibindo');
+}
